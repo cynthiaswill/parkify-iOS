04eb861ea0fb9d863ad6b5fdd99c149f32441eed
cynthiaswill <yzhang4@msn.com>
2022-01-06 00:28:56 +0000

    adds constants

---
+++ constants/Categories.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    category_name: "Running",
+    image_url:
+      "https://www.nicepng.com/png/detail/52-527088_fitness-runner-clip-art-at-clipart-running-figures.png",
+  },
+  {
+    category_name: "Walking",
+    image_url:
+      "https://www.pngitem.com/pimgs/m/47-478531_hiking-clipart-hiker-silhouette-hiking-icon-hd-png.png",
+  },
+  {
+    category_name: "Cycling",
+    image_url:
+      "https://i.pinimg.com/originals/62/22/37/6222375241a8a426b2c3f469daae393a.jpg",
+  },
+  {
+    category_name: "Football",
+    image_url:
+      "https://www.nicepng.com/png/detail/410-4103223_soccer-ball-clipart-black-and-white-9tz6bqnte-png.png",
+  },
+  {
+    category_name: "Picnic",
+    image_url:
+      "https://previews.123rf.com/images/pandavector/pandavector1611/pandavector161100973/65415365-picnic-basket-icon-in-outline-style-isolated-on-white-background-park-symbol-vector-illustration-.jpg",
+  },
+  {
+    category_name: "Party",
+    image_url:
+      "https://thumbs.dreamstime.com/b/line-art-black-white-party-popper-holiday-cracker-props-birthday-themed-vector-illustration-icon-stamp-label-certificate-145299261.jpg",
+  },
+  { category_name: "Other", image_url: "https://i.ibb.co/m6bdV15/logo.jpg" },
+];
---
+++ constants/DrawerItems.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "Home",
+    iconName: "home",
+  },
+  {
+    name: "Find Event",
+    iconName: "search",
+  },
+  {
+    name: "Create Event",
+    iconName: "pen",
+  },
+  {
+    name: "User Page",
+    iconName: "user-circle",
+  },
+  {
+    name: "Login",
+    iconName: "cog",
+  },
+];
