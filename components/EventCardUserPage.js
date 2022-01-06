9698c14fce5f51f1e3b0cf1a74b8f419dda08f2d
cynthiaswill <yzhang4@msn.com>
2022-01-06 00:47:55 +0000

    adds event card user page

---
+++ components/EventCardUserPage.js
@@ -0,0 +1,107 @@
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import { UserContext } from "../contexts/user-context.js";
+
+import { useNavigation } from "@react-navigation/native";
+
+import Categories from "../constants/Categories.js";
+
+export default function EventCardUserPage({ currentEvent }) {
+  // const Stack = createNativeStackNavigator();
+  //   const navigation = useNavigation();
+  console.log(currentEvent);
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.mainContainer}>
+        <Image
+          source={{
+            uri: `${
+              Categories.filter((cat) => cat.category_name === currentEvent.category)[0][
+                "image_url"
+              ]
+            }`,
+          }}
+          style={styles.eventImage}
+        />
+        <View style={styles.textContainer}>
+          <View style={styles.textRow}>
+            <Text style={{ color: "white" }}>
+              {currentEvent.title.length > 25
+                ? `${currentEvent.title.slice(0, 20)}...`
+                : currentEvent.title}
+            </Text>
+            <Text style={{ color: "white" }}>
+              {currentEvent.eventStart.slice(0, 10).replaceAll("-", "/")}
+            </Text>
+          </View>
+          <View style={styles.textRow}>
+            <Text style={{ color: "white" }}>
+              Creator: {currentEvent.creator ? currentEvent.creator.username : null}
+            </Text>
+            <Text style={{ color: "white" }}>
+              {currentEvent.eventStart.slice(11, 16)} -{" "}
+              {currentEvent.eventEnd.slice(11, 16)}
+            </Text>
+          </View>
+          <View style={styles.textRow}>
+            <Text style={{ color: "white" }}>
+              Info:{" "}
+              {currentEvent.description.length > 60
+                ? `${currentEvent.description.slice(0, 55)}...`
+                : currentEvent.description}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "#8E806A",
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "white",
+  },
+  mainContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    borderRadius: 10,
+    borderColor: "lightgrey",
+    borderWidth: 1,
+    height: 90,
+  },
+  eventImage: {
+    flex: 1,
+    borderRadius: 10,
+  },
+  textContainer: {
+    flexDirection: "column",
+    flex: 3,
+  },
+  textRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 3,
+  },
+  extraContainer: {
+    flexDirection: "row",
+    margin: 5,
+  },
+  joinButton: {
+    marginLeft: 65,
+    marginRight: 85,
+  },
+  buttonText: {
+    width: 80,
+    justifyContent: "center",
+  },
+  creatorButtonText: {
+    fontStyle: "italic",
+    textDecorationLine: "underline",
+    color: "blue",
+  },
+});
