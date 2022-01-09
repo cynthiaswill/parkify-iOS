6ed1dba551aa60fae6f152ce50caccbc30a1d124
cynthiaswill <yzhang4@msn.com>
2022-01-09 00:26:22 +0000

    installs os specific material ui

--- components/Container.native.js
+++ components/Container.native.js
@@ -1,3 +1,4 @@
 import { TextField, OutlinedTextField } from "rn-material-ui-textfield";
+import DropDownPicker from "react-native-dropdown-picker";
 
-export default { TextField, OutlinedTextField };
+export default { TextField, OutlinedTextField, DropDownPicker };
