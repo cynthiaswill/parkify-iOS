cfb8326d3e0eb965db5971783fe0506f87c84b90
cynthiaswill <yzhang4@msn.com>
2022-01-11 00:17:24 +0000

    temporarily handles not logged in error in events page

--- screens/Meets.js
+++ screens/Meets.js
@@ -11,7 +11,7 @@ import {
 import EventCard from "../components/EventCard.js";
 //import { getParks, getEvents, getCategories } from "../utils/api.js";
 import { getEvents } from "../utils/nh-api.js";
-// import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ViewEvent } from "./ViewEvent.js";
 import { ViewUser } from "./ViewUser.js";
@@ -36,35 +36,37 @@ export const Meets = () => {
   const [joinedClicked, setJoinedClicked] = useState(false);
   const [events, setEvents] = useState([]);
   const { user } = useContext(UserContext);
-  // const navigation = useNavigation();
+  const navigation = useNavigation();
 
   useEffect(() => {
-    if (joinedClicked) setJoinedClicked(false);
-    getEvents(user.token, categoryValue).then(({ data }) => {
-      const events = [];
+    if (user) {
+      if (joinedClicked) setJoinedClicked(false);
+      getEvents(user.token, categoryValue).then(({ data }) => {
+        const events = [];
 
-      if (joinedValue === "Joined events") {
-        data.events.forEach((event) => {
-          let joined = false;
-          event.participants.forEach((person) => {
-            if (person.username === user.username) joined = true;
+        if (joinedValue === "Joined events") {
+          data.events.forEach((event) => {
+            let joined = false;
+            event.participants.forEach((person) => {
+              if (person.username === user.username) joined = true;
+            });
+            if (joined === true) events.push(event);
           });
-          if (joined === true) events.push(event);
-        });
-      } else if (joinedValue === "Not joined") {
-        data.events.forEach((event) => {
-          let joined = false;
-          event.participants.forEach((person) => {
-            if (person.username === user.username) joined = true;
+        } else if (joinedValue === "Not joined") {
+          data.events.forEach((event) => {
+            let joined = false;
+            event.participants.forEach((person) => {
+              if (person.username === user.username) joined = true;
+            });
+            if (joined === false) events.push(event);
           });
-          if (joined === false) events.push(event);
-        });
-      } else {
-        data.events.forEach((event) => events.push(event));
-      }
+        } else {
+          data.events.forEach((event) => events.push(event));
+        }
 
-      setEvents(events);
-    });
+        setEvents(events);
+      });
+    }
   }, [categoryValue, joinedValue, joinedClicked]);
 
   const [mapOpened, setMapOpened] = useState(true);
@@ -72,44 +74,45 @@ export const Meets = () => {
   const Stack = createNativeStackNavigator();
 
   const MeetsPage = () => {
-    return (
-      <View style={styles.wholePage}>
-        {/* <ImageBackground
+    if (user) {
+      return (
+        <View style={styles.wholePage}>
+          {/* <ImageBackground
           source={"https://i.makeagif.com/media/8-27-2015/1NpjsX.gif"}
           resizeMode="cover"
           style={styles.background}
         > */}
-        <View style={styles.topSelectorRow}>
-          <Picker
-            style={styles.pickerStyle}
-            selectedValue={categoryValue}
-            onValueChange={(itemValue, itemIndex) => {
-              setCategoryValue(itemValue);
-            }}
-          >
-            <Picker.Item
-              key="All categories"
-              label="All categories"
-              value="All categories"
-            />
-            {Categories.map((cat) => {
-              return (
-                <Picker.Item
-                  key={cat.category_name}
-                  label={cat.category_name}
-                  value={cat.category_name}
-                />
-              );
-            })}
-          </Picker>
-          {/* <TextInput
+          <View style={styles.topSelectorRow}>
+            <Picker
+              style={styles.pickerStyle}
+              selectedValue={categoryValue}
+              onValueChange={(itemValue, itemIndex) => {
+                setCategoryValue(itemValue);
+              }}
+            >
+              <Picker.Item
+                key="All categories"
+                label="All categories"
+                value="All categories"
+              />
+              {Categories.map((cat) => {
+                return (
+                  <Picker.Item
+                    key={cat.category_name}
+                    label={cat.category_name}
+                    value={cat.category_name}
+                  />
+                );
+              })}
+            </Picker>
+            {/* <TextInput
             style={styles.dateInput}
             value={eventDate}
             onChangeText={setEventDate}
             placeholder="DD/MM/YYYY"
           /> */}
 
-          {/* <DatePicker
+            {/* <DatePicker
             defaultDate={new Date(2021, 4, 4)}
             minimumDate={new Date(2021, 1, 1)}
             maximumDate={new Date(2021, 12, 31)}
@@ -127,82 +130,88 @@ export const Meets = () => {
           />
           <Text>{eventDate.toString().substr(4, 12)}</Text> */}
 
-          <Picker
-            style={styles.pickerStyle}
-            selectedValue={joinedValue}
-            onValueChange={(itemValue, itemIndex) => setJoinedValue(itemValue)}
-          >
-            {joinedOptions.map((opt) => {
-              return <Picker.Item key={opt.label} label={opt.label} value={opt.value} />;
-            })}
-          </Picker>
-        </View>
-        <View style={styles.secondRowContainer}>
-          <Pressable
-            style={styles.button}
-            onPress={() => {
-              if (mapOpened) {
-                setMapOpened(false);
-              } else {
-                setMapOpened(true);
-              }
-            }}
-          >
+            <Picker
+              style={styles.pickerStyle}
+              selectedValue={joinedValue}
+              onValueChange={(itemValue, itemIndex) => setJoinedValue(itemValue)}
+            >
+              {joinedOptions.map((opt) => {
+                return (
+                  <Picker.Item key={opt.label} label={opt.label} value={opt.value} />
+                );
+              })}
+            </Picker>
+          </View>
+          <View style={styles.secondRowContainer}>
+            <Pressable
+              style={styles.button}
+              onPress={() => {
+                if (mapOpened) {
+                  setMapOpened(false);
+                } else {
+                  setMapOpened(true);
+                }
+              }}
+            >
+              {mapOpened ? (
+                <Text style={styles.buttonText}>Collapse map</Text>
+              ) : (
+                <Text style={styles.buttonText}>Open map</Text>
+              )}
+            </Pressable>
             {mapOpened ? (
-              <Text style={styles.buttonText}>Collapse map</Text>
-            ) : (
-              <Text style={styles.buttonText}>Open map</Text>
-            )}
-          </Pressable>
-          {mapOpened ? (
-            <View>
-              <View style={styles.mapContainer}>
-                <MapView
-                  style={styles.map}
-                  initialRegion={{
-                    latitude: 53.47791641806832,
-                    longitude: -2.242188787189367,
-                    latitudeDelta: 0.4522,
-                    longitudeDelta: 1.1421,
-                  }}
-                >
-                  {events.map((eachEvent) => {
-                    return (
-                      <MapView.Marker
-                        key={eachEvent.title}
-                        title={eachEvent.title}
-                        description={eachEvent.description}
-                        coordinate={{
-                          latitude: eachEvent.location.latitude,
-                          longitude: eachEvent.location.longitude,
-                        }}
-                      />
-                    );
-                  })}
-                </MapView>
+              <View>
+                <View style={styles.mapContainer}>
+                  <MapView
+                    style={styles.map}
+                    initialRegion={{
+                      latitude: 53.47791641806832,
+                      longitude: -2.242188787189367,
+                      latitudeDelta: 0.4522,
+                      longitudeDelta: 1.1421,
+                    }}
+                  >
+                    {events.map((eachEvent) => {
+                      return (
+                        <MapView.Marker
+                          key={eachEvent.title}
+                          title={eachEvent.title}
+                          description={eachEvent.description}
+                          coordinate={{
+                            latitude: eachEvent.location.latitude,
+                            longitude: eachEvent.location.longitude,
+                          }}
+                        />
+                      );
+                    })}
+                  </MapView>
+                </View>
               </View>
-            </View>
-          ) : null}
+            ) : null}
+          </View>
+          <ScrollView>
+            {events.length === 0 ? (
+              <Text>No events here</Text>
+            ) : (
+              events.map((currentEvent) => {
+                return (
+                  <EventCard
+                    key={currentEvent._id}
+                    // navigation={navigation}
+                    currentEvent={currentEvent}
+                    setJoinedClicked={setJoinedClicked}
+                  />
+                );
+              })
+            )}
+          </ScrollView>
+          {/* </ImageBackground> */}
         </View>
-        <ScrollView>
-          {events.length === 0 ? (
-            <Text>No events here</Text>
-          ) : (
-            events.map((currentEvent) => {
-              return (
-                <EventCard
-                  key={currentEvent._id}
-                  // navigation={navigation}
-                  currentEvent={currentEvent}
-                  setJoinedClicked={setJoinedClicked}
-                />
-              );
-            })
-          )}
-        </ScrollView>
-        {/* </ImageBackground> */}
-      </View>
-    );
+      );
+    } else {
+      navigation.navigate("Login");
+      return <Text>You are not logged in!</Text>;
+    }
   };
 
   return (
