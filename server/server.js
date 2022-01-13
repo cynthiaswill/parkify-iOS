f139dc982a7bca2cfe6f7cbcacb23366d9441d9c
cynthiaswill <yzhang4@msn.com>
2022-01-13 07:56:09 +0000

    cleanup chat server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const socket = require("socket.io");
-
 const { get_Current_User, user_Disconnect, join_User } = require("./socketUser");
-
 const app = express();
 app.use(cors());
 const { MongoClient } = require("mongodb");
-// const uri =
-//   "mongodb+srv://yzhang4:123456abc@cluster0.rspyf.mongodb.net/My_test_project?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const uri =
   "mongodb+srv://balli2:balli2@frosty.wq0ip.mongodb.net/frosty?retryWrites=true&w=majority";
@@ -17,11 +12,6 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 app.use(express());
 
@@ -39,27 +29,6 @@ io.on("connection", (socket) => {
     const p_user = join_User(socket.id, username, title);
     console.log(socket.id, "=id");
     socket.join(p_user.title);
-
-    // // fetching chat history of this room
-    // fetchHistory(p_user.title)
-    //   .then((data) => console.log(data, "!!"))
-    //   .catch(console.dir);
-
-    // console.log("fetching history step");
-
-    //display a welcome message to the user who have joined a room
-    // socket.emit("message", {
-    //   userId: p_user.id,
-    //   username: p_user.username,
-    //   messageBody: `Welcome ${p_user.username}`,
-    // });
-
-    // //displays a joined room message to all other room users except that particular user
-    // socket.broadcast.to(p_user.title).emit("message", {
-    //   userId: p_user.id,
-    //   username: p_user.username,
-    //   messageBody: `${p_user.username} has joined the chat`,
-    // });
   });
 
   //listener#2: user sending message
@@ -111,14 +80,3 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-// app.get(`/history/:room`, (req, res) => {
-//   const { room } = req.params;
-//   fetchHistory(room)
-//     .then((data) => {
-//       return data;
-//     })
-//     .then((history) => {
-//       res.send({ history });
-//     });
-// });
